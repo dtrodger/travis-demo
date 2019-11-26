580ad9adf022f8fc8f99a2ebbf6b82d141adcb60
boxdave <drodgers@box.com>
2019-11-25 19:01:05 -0600

    change app text

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,3 @@ RUN npm run build
 FROM nginx
 EXPOSE 80
 COPY --from=builder /usr/web-client-react-1/build /usr/share/nginx/html
-
-
-
--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          AWS Elastic Beanstalk Deployment
         </a>
       </header>
     </div>
